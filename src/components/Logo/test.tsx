07b149e16ea4ca761e3ed0458092f493439f380d
yago-codev <yagomilano92@gmail.com>
2023-01-28 14:14:26 -0300

    test: criando testes iniciais

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -2,16 +2,28 @@ import { render, screen } from "@testing-library/react";
 
 import Logo from ".";
 
+import { renderWithTheme } from "utils/tests/helpers";
+
 describe("<Logo />", () => {
   // deve renderizar a logo branca por padrão
   it("should render a white label by default", () => {
     // 1) renderizar o componente `container`
-    const { container } = render(<Logo />);
+    // 1.1) como estamos usando um tema para aplicação utilizando styled-components,
+    // precisaremos renderizar o Logo dentro do helper de testes
+    renderWithTheme(<Logo />);
     // 2) selecionar o elemento a ser testado com o método `screen`
     // 2.1) através do método `screen` conseguiremos fazer "queries": getByLabel...
     // 3) expect - assertion: asserção - comparação -> espero que determinada coisa ocorra...
-    expect(screen.getByRole("heading", { name: /Logo/i })).toBeInTheDocument();
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: "#FAFAFA",
+    });
+  });
 
-    expect(container.firstChild).toMatchSnapshot();
+  // deve renderizar a logo preta quando a color for passada
+  it("should render a black label when color is passed", () => {
+    renderWithTheme(<Logo color="black" />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: "#030517",
+    });
   });
 });
